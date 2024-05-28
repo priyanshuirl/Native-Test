706f363e9169fb30faa6bc0091ccc1a77b555e64
Priyanshu Native <priyanshunative@MacBook-Pro.local>
2024-05-28 15:48:46 +0530

    Build error fix

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -3,9 +3,9 @@ import lineIcon from "./lineIcon.svg";
 import xIcon from "./xIcon.svg";
 import nativeForLove from "./nativeForLove.svg";
 import nativeForCareer from "./nativeForCareer.svg";
-import instaIconlarge from "./instaIconlarge.svg";
-import lineIconlarge from "./lineIconlarge.svg";
-import xIconlarge from "./xIconlarge.svg";
+import instaIconlarge from "./instaIconLarge.svg";
+import lineIconlarge from "./lineIconLarge.svg";
+import xIconlarge from "./xIconLarge.svg";
 
 export {
   instaIcon,
